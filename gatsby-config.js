768f44b8700cf7ce9a223b1b33270ae49ca887ac
Saumya Kanoria <svk@humbleschool.in>
2021-01-11 15:15:26 +0530

    Configures for deployment to S3 and Cloudfront

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const siteAddress = new URL("https://www.dialogform.in");
+
 module.exports = {
   siteMetadata: {
     title: `DialogForm`,
@@ -30,8 +32,28 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    //
+
     // MYMOTE: Everything below added by us
+
     `gatsby-plugin-sass`,
+    // We followed instructions on
+    // https://www.gatsbyjs.com/docs/how-to/previews-deploys-hosting/deploying-to-s3-cloudfront/
+    // to deploy to AWS S3 and Cloudfront.
+    // We also followed instructions and optimization tips for Cloudfront on
+    // https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/with-cloudfront.md
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "hssvk-chatbot-website",
+        protocol: siteAddress.protocol.slice(0, -1),
+        hostname: siteAddress.hostname,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: siteAddress.href.slice(0, -1),
+      },
+    },
   ],
 };

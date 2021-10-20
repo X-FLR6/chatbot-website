83a85fdd60b8a97ca6a0ffb01d9d5b44c7b7e2c1
Saumya Kanoria <svk@humbleschool.in>
2021-10-20 16:35:37 +0530

    Extracts out PricingPlansSection component, it was getting too distributed

---
+++ src/components/pricing-plans-section.js
@@ -0,0 +1,139 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+import PricingPlan from "./pricing-plan";
+
+function NoServiceInterruptionModal({ isActive, onCloseClick }) {
+  return (
+    <div className={classNames("modal", { "is-active": isActive })}>
+      <div className="modal-background" onClick={onCloseClick} />
+      <div className="modal-content">
+        <div className="card">
+          <div className="card-content">
+            <div className="block">
+              If you go above the monthly chat limit, we will contact you to
+              upgrade your plan. Until your plan is upgraded, you will be
+              continue to be charged at the rate of your current plan, no matter
+              how much you go over the limit, and your service will not be
+              interrupted.
+            </div>
+            <div className="block">
+              We trust that you will work with us to upgrade your plan as and
+              when necessary.
+            </div>
+          </div>
+          <footer className="card-footer">
+            <a
+              href="#"
+              className="card-footer-item"
+              onClick={(event) => {
+                event.preventDefault();
+                onCloseClick();
+              }}
+            >
+              Close
+            </a>
+          </footer>
+        </div>
+      </div>
+      <button
+        className="modal-close is-large"
+        aria-label="close"
+        onClick={onCloseClick}
+      />
+    </div>
+  );
+}
+
+NoServiceInterruptionModal.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  onCloseClick: PropTypes.func.isRequired,
+};
+
+function PricingPlansSection() {
+  const [
+    isNoServiceInterruptionModalActive,
+    setIsNoServiceInterruptionModalActive,
+  ] = useState(false);
+
+  const COMMON_FEATURES = [
+    <div key="no-service-interruption">
+      <div className="has-text-success-dark">
+        No service interruption if limit crossed
+      </div>
+      <button
+        className="button is-small is-fullwidth is-success is-light mt-1"
+        style={{ height: "1.5em" }}
+        onClick={() => setIsNoServiceInterruptionModalActive(true)}
+      >
+        How does this work?
+      </button>
+    </div>,
+    "#Common to all plans",
+    "Unlimited content",
+    "Unlimited branching logic",
+    "Auto max-score calculation",
+    "Share content via link",
+    "Embed content in any website",
+    "View & analyze conversations",
+    "Analyze drop-off rates",
+    "Scoring algorithms to suit various outcomes",
+    "Give feedback at any point in a conversation",
+  ];
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h3 className="title has-text-centered">Pricing Plans</h3>
+        <div className="columns is-centered">
+          <div className="column is-4 is-3-widescreen">
+            <PricingPlan
+              name="Free"
+              price="Free"
+              interval="forever"
+              features={["100 chats/month", ...COMMON_FEATURES]}
+            />
+          </div>
+          <div className="column is-4 is-3-widescreen">
+            <PricingPlan
+              name="Basic"
+              currency="Rs."
+              price="2000"
+              interval="month"
+              features={["1000 chats/month", ...COMMON_FEATURES]}
+            />
+          </div>
+          <div className="column is-4 is-3-widescreen">
+            <PricingPlan
+              name="Plus"
+              currency="Rs."
+              price="5000"
+              interval="month"
+              features={["5000 chats/month", ...COMMON_FEATURES]}
+            />
+          </div>
+        </div>
+        <div className="columns is-centered">
+          <div className="column is-12 is-9-widescreen">
+            <div className="card has-background-info-light">
+              <div className="card-content">
+                To purchase a plan, to design a custom plan to suit your needs,
+                or for any other questions,
+                <br />
+                write to us at{" "}
+                <a href="mailto:hi@dialogform.in">hi@dialogform.in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <NoServiceInterruptionModal
+        isActive={isNoServiceInterruptionModalActive}
+        onCloseClick={() => setIsNoServiceInterruptionModalActive(false)}
+      />
+    </section>
+  );
+}
+
+export default PricingPlansSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,99 +2,21 @@ import "./mystyles.scss";
 
 import classNames from "classnames";
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 import { BsChatQuoteFill } from "react-icons/bs";
 
 import AdminScreenshot from "../components/admin-screenshot";
 import DemoChatButton from "../components/demo-chat-button";
 import Layout from "../components/layout";
 import MemberProfile from "../components/member-profile";
-import PricingPlan from "../components/pricing-plan";
+import PricingPlansSection from "../components/pricing-plans-section";
 import SEO from "../components/seo";
 import amarnathPic from "../images/amarnath.png";
 import rajatPic from "../images/rajat.png";
 import saumyaPic from "../images/saumya.png";
 import styles from "./index.module.css";
 
-function NoServiceInterruptionModal({ isActive, onCloseClick }) {
-  return (
-    <div className={classNames("modal", { "is-active": isActive })}>
-      <div className="modal-background" onClick={onCloseClick} />
-      <div className="modal-content">
-        <div className="card">
-          <div className="card-content">
-            <div className="block">
-              If you go above the monthly chat limit, we will contact you to
-              upgrade your plan. Until your plan is upgraded, you will be
-              continue to be charged at the rate of your current plan, no matter
-              how much you go over the limit, and your service will not be
-              interrupted.
-            </div>
-            <div className="block">
-              We trust that you will work with us to upgrade your plan as and
-              when necessary.
-            </div>
-          </div>
-          <footer className="card-footer">
-            <a
-              href="#"
-              className="card-footer-item"
-              onClick={(event) => {
-                event.preventDefault();
-                onCloseClick();
-              }}
-            >
-              Close
-            </a>
-          </footer>
-        </div>
-      </div>
-      <button
-        className="modal-close is-large"
-        aria-label="close"
-        onClick={onCloseClick}
-      />
-    </div>
-  );
-}
-
-NoServiceInterruptionModal.propTypes = {
-  isActive: PropTypes.bool.isRequired,
-  onCloseClick: PropTypes.func.isRequired,
-};
-
 function IndexPage() {
-  const [
-    isNoServiceInterruptionModalActive,
-    setIsNoServiceInterruptionModalActive,
-  ] = useState(false);
-
-  const COMMON_FEATURES = [
-    <div key="no-service-interruption">
-      <div className="has-text-success-dark">
-        No service interruption if limit crossed
-      </div>
-      <button
-        className="button is-small is-fullwidth is-success is-light mt-1"
-        style={{ height: "1.5em" }}
-        onClick={() => setIsNoServiceInterruptionModalActive(true)}
-      >
-        How does this work?
-      </button>
-    </div>,
-    "#Common to all plans",
-    "Unlimited content",
-    "Unlimited branching logic",
-    "Auto max-score calculation",
-    "Share content via link",
-    "Embed content in any website",
-    "View & analyze conversations",
-    "Analyze drop-off rates",
-    "Scoring algorithms to suit various outcomes",
-    "Give feedback at any point in a conversation",
-  ];
-
   return (
     <Layout>
       <SEO title="Home" />
@@ -229,51 +151,7 @@ function IndexPage() {
           </a>
         </div>
       </section>
-      <section className="section">
-        <div className="container">
-          <h3 className="title has-text-centered">Pricing</h3>
-          <div className="columns is-centered">
-            <div className="column is-4 is-3-widescreen">
-              <PricingPlan
-                name="Free"
-                price="Free"
-                features={["100 chats/month", ...COMMON_FEATURES]}
-              />
-            </div>
-            <div className="column is-4 is-3-widescreen">
-              <PricingPlan
-                name="Basic"
-                currency="Rs."
-                price="2000"
-                interval="month"
-                features={["1000 chats/month", ...COMMON_FEATURES]}
-              />
-            </div>
-            <div className="column is-4 is-3-widescreen">
-              <PricingPlan
-                name="Plus"
-                currency="Rs."
-                price="5000"
-                interval="month"
-                features={["5000 chats/month", ...COMMON_FEATURES]}
-              />
-            </div>
-          </div>
-          <div className="columns is-centered">
-            <div className="column is-12 is-9-widescreen">
-              <div className="card has-background-info-light">
-                <div className="card-content">
-                  To purchase a plan, to design a custom plan to suit your
-                  needs, or for any other questions,
-                  <br />
-                  write to us at{" "}
-                  <a href="mailto:hi@dialogform.in">hi@dialogform.in</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <PricingPlansSection />
       <footer className="footer is-flex is-justify-content-center">
         <a
           className="is-link mx-3 is-uppercase"
@@ -291,10 +169,6 @@ function IndexPage() {
           Privacy
         </Link>
       </footer>
-      <NoServiceInterruptionModal
-        isActive={isNoServiceInterruptionModalActive}
-        onCloseClick={() => setIsNoServiceInterruptionModalActive(false)}
-      />
     </Layout>
   );
 }
